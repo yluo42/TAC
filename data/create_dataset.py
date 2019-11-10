62a37551475c8bd00dd01b7de6e5f05acf48d729
yluo42 <yluo42@users.noreply.github.com>
2019-11-10 18:21:15 -0500

    Update create_dataset.py

--- data/create_dataset.py
+++ data/create_dataset.py
@@ -26,11 +26,6 @@ def generate_data(output_path='', dataset='adhoc', libri_path='/hdd/data/Librisp
         sr = 16000
         # signal length is 4 sec
         sig_len = 4
-        
-        # generate and save audio
-        save_dir = os.path.join(output_path, 'MC_Libri_'+dataset, data_type[i])
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
                 
         for utt in range(len(configs)):
             this_config = configs[utt]
@@ -42,21 +37,32 @@ def generate_data(output_path='', dataset='adhoc', libri_path='/hdd/data/Librisp
             spk2, _ = sf.read(os.path.join(libri_path, speakers[1]))
             noise, _ = sf.read(os.path.join(noise_path, noise))
             
+            
+            
             # calculate signal length according to overlap ratio
             overlap_ratio = this_config['overlap_ratio']
-            actual_len = int(sig_len / (2 - overlap_ratio)) * sr
+            actual_len = int(sig_len / (2 - overlap_ratio) * sr)
             overlap = int(actual_len*overlap_ratio)
             
             # truncate speech according to start and end indexes
             start_idx = this_config['start_idx']
-            end_idx = this_config['end_idx']
+            end_idx = start_idx + actual_len
             spk1 = spk1[start_idx:end_idx]
             spk2 = spk2[start_idx:end_idx]
             
-            # rescaling spk2 energy according to relative SNR
+            # rescaling speaker and noise energy according to relative SNR
             spk1 = spk1 / np.sqrt(np.sum(spk1**2)+1e-8) * 1e2
             spk2 = spk2 / np.sqrt(np.sum(spk2**2)+1e-8) * 1e2
             spk2 = spk2 * np.power(10, this_config['spk_snr']/20.)
+            # repeat noise if necessary
+            noise = noise[:int(sig_len*sr)]
+            if len(noise) < int(sig_len*sr):
+                num_repeat = int(sig_len*sr) // len(noise)
+                res = int(sig_len*sr) - num_repeat * len(noise)
+                noise = np.concatenate([np.concatenate([noise]*num_repeat), noise[:res]])
+            # rescale noise energy w.r.t mixture energy
+            noise = noise / np.sqrt(np.sum(noise**2)+1e-8) * np.sqrt(np.sum((spk1+spk2)**2)+1e-8)
+            noise = noise / np.power(10, this_config['noise_snr']/20.)
             
             # load locations and room configs
             mic_pos = np.asarray(this_config['mic_pos'])
@@ -64,6 +70,7 @@ def generate_data(output_path='', dataset='adhoc', libri_path='/hdd/data/Librisp
             noise_pos = np.asarray(this_config['noise_pos'])
             room_size = np.asarray(this_config['room_size'])
             rt60 = this_config['RT60']
+            num_mic = len(mic_pos)
             
             # generate RIR
             beta = gpuRIR.beta_SabineEstimation(room_size, rt60)
@@ -72,50 +79,48 @@ def generate_data(output_path='', dataset='adhoc', libri_path='/hdd/data/Librisp
             noise_rir = gpuRIR.simulateRIR(room_size, beta, noise_pos, mic_pos, nb_img, rt60, sr)
             
             # convolve with RIR at different mic
-            if dataset == 'adhoc':
-                nmic = this_config['num_mic']
-            else:
-                nmic = 6
+            echoic_spk1 = []
+            echoic_spk2 = []
+            echoic_mixture = []
+            
+            nmic = this_config['num_mic']
             for mic in range(nmic):
                 spk1_echoic_sig = signal.fftconvolve(spk1, spk_rir[0][mic])
                 spk2_echoic_sig = signal.fftconvolve(spk2, spk_rir[1][mic])
+                noise_echoic_sig = signal.fftconvolve(noise, noise_rir[0][mic])
                 
                 # align the speakers according to overlap ratio
-                actual_length = len(spk1_echoic_sig)
-                total_length = actual_length * 2 - overlap
-                padding = np.zeros(actual_length - overlap)
+                pad_length = int((1 - overlap_ratio) * actual_len)
+                padding = np.zeros(pad_length)
                 spk1_echoic_sig = np.concatenate([spk1_echoic_sig, padding])
                 spk2_echoic_sig = np.concatenate([padding, spk2_echoic_sig])
-                mixture = spk1_echoic_sig + spk2_echoic_sig
+                # pad or truncate length to 4s if necessary
+                def pad_sig(x):
+                    if len(x) < sig_len*sr:
+                        zeros = np.zeros(sig_len * sr - len(x))
+                        return np.concatenate([x, zeros])
+                    else:
+                        return x[:sig_len*sr]
+                    
+                spk1_echoic_sig = pad_sig(spk1_echoic_sig)
+                spk2_echoic_sig = pad_sig(spk2_echoic_sig)
+                noise_echoic_sig = pad_sig(noise_echoic_sig)
                 
-                # add noise
-                noise = noise[:total_length]
-                if len(noise) < total_length:
-                    # repeat noise if necessary
-                    num_repeat = total_length // len(noise)
-                    res = total_length - num_repeat * len(noise)
-                    noise = np.concatenate([np.concatenate([noise]*num_repeat), noise[:res]])
-                noise = signal.fftconvolve(noise, noise_rir[0][mic])
-
-                # rescaling noise energy
-                noise = noise[:total_length]
-                noise = noise / np.sqrt(np.sum(noise**2)+1e-8) * np.sqrt(np.sum(mixture**2)+1e-8)
-                noise = noise / np.power(10, this_config['noise_snr']/20.)
-
-                mixture += noise
+                # sum up for mixture
+                mixture = spk1_echoic_sig + spk2_echoic_sig + noise_echoic_sig
             
                 # save waveforms
-                this_save_dir = os.path.join(save_dir, str(nmic)+'mic', 'sample'+str(utt+1))
+                this_save_dir = os.path.join(output_path, 'MC_Libri_'+dataset, data_type[i], str(num_mic)+'mic', 'sample'+str(utt+1))
                 if not os.path.exists(this_save_dir):
                     os.makedirs(this_save_dir)
                 sf.write(os.path.join(this_save_dir, 'spk1_mic'+str(mic+1)+'.wav'), spk1_echoic_sig, sr)
                 sf.write(os.path.join(this_save_dir, 'spk2_mic'+str(mic+1)+'.wav'), spk2_echoic_sig, sr)
                 sf.write(os.path.join(this_save_dir, 'mixture_mic'+str(mic+1)+'.wav'), mixture, sr)
                 
-            # print progress
-            if (utt+1) % (len(configs) // 5) == 0:
-                print("{} configuration, {} set, {:d} out of {:d} utterances generated.".format(dataset, data_type[i],
-                                                                                                utt+1, len(configs)))
+                # print progress
+                if (utt+1) % (len(configs) // 5) == 0:
+                    print("{} configuration, {} set, {:d} out of {:d} utterances generated.".format(dataset, data_type[i],
+                                                                                                    utt+1, len(configs)))
 
 
 if __name__ == "__main__":
